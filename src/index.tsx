8170cf5ce83ceff8a416452b8691cf2e9ae64408
nanami69 <69er.11.11@gmail.com>
2021-04-21 22:55:35 +0900

    create: Didact.createElement

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Didact = {
+  createElement,
+};
+
+/** @jsx Didact.createElement */
 const element = (
   <div id="foo">
     <a>bar</a>
     <b />
   </div>
-)
-const container = document.getElementById("root")
-ReactDOM.render(element, container)
+);
+const container = document.getElementById("root");
+ReactDOM.render(element, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+function createElement(type: any, props: any, ...children: any) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child: any) =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+
+function createTextElement(text: string) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
