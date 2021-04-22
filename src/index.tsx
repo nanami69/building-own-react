3b9a4003f4305bf3e70639f09a53467a4fb0cbab
nanami69 <69er.11.11@gmail.com>
2021-04-22 22:46:01 +0900

    step 3: after

--- src/index.tsx
+++ src/index.tsx
@@ -38,6 +38,23 @@ if ( container ) {
   Didact.render(element, container);
 }
 
+let nextUnitOfWork: any = null;
+​
+function workLoop(deadline: IdleDeadline): void {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop)
+}
+​
+requestIdleCallback(workLoop)
+​
+function performUnitOfWork(nextUnitOfWork: any) {
+  // TODO
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
