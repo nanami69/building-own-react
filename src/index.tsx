8945ca91df7fb83f95b5019bf5873d2243f09cd6
nanami69 <69er.11.11@gmail.com>
2021-04-25 10:39:18 +0900

    step6 after

--- src/index.tsx
+++ src/index.tsx
@@ -25,11 +25,15 @@ function render(element: any, container: HTMLElement) {
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   }
+  deletions = [];
   nextUnitOfWork = wipRoot;
 }
 
 let wipRoot: any = null;
+let currentRoot: any = null;
+let deletions: any = null;
 
 const Didact = {
   createElement,
@@ -52,17 +56,79 @@ if ( container ) {
 
 function commitRoot() {
   commitWork(wipRoot.child);
+  currentRoot = wipRoot;
   wipRoot = null;
+}
+
+const isEvent = (key: string) => key.startsWith("on");
+const isProperty = (key: string) => key !== "children" && !isEvent(key);
+const isNew = (prev: any, next: any) => (key: string) => prev[key] !== next[key];
+const isGone = (prev: any, next: any) => (key: string) => !(key in next);
+function updateDom(dom: any, prevProps: any, nextProps: any) {
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(
+      key =>
+        !(key in nextProps) ||
+        isNew(prevProps, nextProps)(key)
+    )
+    .forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.removeEventListener(
+        eventType,
+        prevProps[name]
+      )
+    });
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ""
+    });
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+  .filter(isEvent)
+  .filter(isNew(prevProps, nextProps))
+  .forEach(name => {
+    const eventType = name
+      .toLowerCase()
+      .substring(2)
+    dom.addEventListener(
+      eventType,
+      nextProps[name]
+    )
+  });
 }
 
 function commitWork(fiber: any) {
   if (!fiber) {
-    return
+    return;
   }
-  const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
-  commitWork(fiber.child)
-  commitWork(fiber.sibling)
+  const domParent = fiber.parent.dom;
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom);
+  }
+  
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
 }
 
 function workLoop(deadline: any): void {
@@ -88,28 +154,8 @@ function performUnitOfWork(fiber: any) {
   }
 
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-
-  while (index < elements.length) {
-    const element = elements[index];
-    const newFiber: any = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-      sibling: null,
-    };
-
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-    prevSibling = newFiber;
-    index++;
-  }
-
+  reconcileChildren(fiber, elements);
+ 
   if (fiber.child) {
     return fiber.child;
 　}
@@ -120,6 +166,56 @@ function performUnitOfWork(fiber: any) {
   }
   nextFiber = nextFiber.parent;
 　}
+}
+
+function reconcileChildren(wipFiber: any, elements: any) {
+  let index = 0;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    let newFiber: any = null;
+
+    const sameType = oldFiber && element && element.type == oldFiber.type;
+
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
 }
 
 // If you want to start measuring performance in your app, pass a function
